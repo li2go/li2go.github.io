dbc1a860b3a25b46f2170fa4b4ab9a9eef00f781
li2go <1055998753@qq.com>
2016-01-23 16:36:45 +0800

    #update

--- articl/articleControl.js
+++ articl/articleControl.js
@@ -1,3 +1,37 @@
 /**
  * Created by lanhao on 16/1/23.
  */
+
+const btnId = {
+    showMask: 'btn_showMask',
+    closeMask: 'btn_closeMask'
+};
+const viewId = {
+    mask: 'fullscreenmask'
+};
+
+
+var controls = {
+    showMask: function () {
+        document.querySelector('#'+viewId.mask).className = 'fullScreenMask show';
+    }, closeMask: function () {
+        document.querySelector('#'+viewId.mask).className = 'fullScreenMask';
+    }
+};
+var dealFunction = function (e) {
+    var el = e.target;
+    switch (el.id) {
+        case btnId.showMask:
+            controls.showMask();
+            break;
+        case btnId.closeMask:
+            controls.closeMask();
+
+            break;
+        default:
+            break;
+    }
+};
+window.addEventListener('click', dealFunction);
+
+
